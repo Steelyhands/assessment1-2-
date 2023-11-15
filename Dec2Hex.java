55d050e078026f14f96709e46a65f36cd5939929
“steelyhands” <acole300@caledonian.ac.uk>
2023-11-15 20:32:05 +0000

    changed comment

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -36,7 +36,7 @@ class Dec2Hex
         System.out.println("error, non convertable data entered");
       }
         //printing end statement for program no matter what try catch does, 
-	//ending program as am not looping till i get a correct input
+	//as program will not loop round after initial input 
         finally
         {
          system.out.println("end of program");
