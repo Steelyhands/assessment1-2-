4d3248b998a7fd3d47202d58014d078d53517241
“steelyhands” <acole300@caledonian.ac.uk>
2023-11-15 19:29:11 +0000

    added comments

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -7,17 +7,17 @@ public class Dec2HexTest {
     public Dec2HexTest()
    {}
     Dec2Hex Dec2Hex;
-
+//gets run before every test creates new Dec2Hex
     @Before
     public void setUp() {
         Dec2Hex = new Dec2Hex();
     }
-
+//testing for calculation against Hex answer that is known to be correct
     @Test
     public void testCalculate() {
         assertEquals("Regular calculation should work",30, Dec2Hex.Arg1(48));
     }
-
+//testing against 0
     @Test
     public void testCalculateWithZero() {
         assertEquals("Calculation with zero should be zero", 0, Dec2Hex.Arg1(0));
